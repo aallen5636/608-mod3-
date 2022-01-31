d6130c4a0e79a7b7d9dfe1eda1bb1fa058cb9451
Ashley Allen <95989498+aallen5636@users.noreply.github.com>
2022-01-30 19:38:09 -0600

    Update and rename README.md to bult-in-min-max.py

---
+++ bult-in-min-max.py
@@ -0,0 +1,30 @@
+def maximum(value1, value2, value3):
+    """Return the maximum of three value Ashley Allen"""
+    max_value = value1
+    if value2 > max_value:
+        max_value = value2
+    if value3 > max_value:
+        max_value = value3
+    return print (max_value)
+[6]:
+
+maximum(12, 27, 36)
+36
+[8]:
+
+maximum(12.3, 45.6, 9.7)
+45.6
+[9]:
+
+maximum('yellow', 'red', 'orange')
+yellow
+[10]:
+
+max(12, 27, 36)
+[10]:
+36
+[12]:
+
+min(15, 9, 27, 14)
+[12]:
+9
